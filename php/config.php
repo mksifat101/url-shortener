3346f75006cda8edb1c31736d9984bd7b8f2c89e
mksifat101 <mksifat101@gmail.com>
2021-12-19 22:42:35 +0600

    Upload All Files

---
+++ php/config.php
@@ -0,0 +1,20 @@
+<?php 
+    /* if you're working on localhost then you don't need to change anything
+       but if you're thinking to upload it to live server then you've to edit something
+    1. Paste your website url with forward slash(/) in the domain variable, you don't need 
+       to write https://wwww. before the domain name if you have domain redirection setup
+    2. Change user, pass, db values accordingly mentioned in the comments below
+    3. Go to JavaScript file and search this keyword - let domain - then paste your url there 
+    4. After all changes you've to wait because javascript file save changes may take time to reflect */ 
+
+    $domain = "https://multi-service.xyz/url_shortener/"; //like this: codingnepalweb.com/
+    $host = "localhost";
+    $user = "multiser_urls"; //Database username
+    $pass = "&1CyTBVw2R6+"; //Database password
+    $db = "multiser_urls"; //Database name
+
+    $conn = mysqli_connect($host, $user, $pass, $db);
+    if(!$conn){
+        echo "Database connection error".mysqli_connect_error();
+    }
+?>
